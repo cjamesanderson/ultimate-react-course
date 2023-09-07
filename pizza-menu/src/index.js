0fc89a825a093ec441fea545bd530637f359f092
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-09-07 11:47:41 -0700

    conditional formatting with ternaries

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -77,12 +77,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObject={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. PLease come back later.</p>
       )}
     </main>
   );
@@ -110,11 +112,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
