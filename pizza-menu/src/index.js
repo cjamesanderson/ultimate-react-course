88ae5c99f6189a33bb6c94e004c950ffdc34fe16
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-09-07 13:53:41 -0700

    conditional classes and text

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -98,15 +98,16 @@ function Menu() {
 }
 
 function Pizza({ pizzaObject }) {
-  if (pizzaObject.soldOut) return null;
+  // if (pizzaObject.soldOut) return null;
 
   return (
-    <li className="pizza">
+    // <li className={pizzaObject.soldOut ? "pizza sold-out" : "pizza"}>
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name}></img>
       <div>
         <h3>{pizzaObject.name}</h3>
         <p>{pizzaObject.ingredients}</p>
-        <span>{pizzaObject.price + 3}</span>
+        <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span>
       </div>
     </li>
   );
