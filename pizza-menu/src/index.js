c54899afb7656874fa1ea087d400d56234f89ba0
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-09-07 11:51:06 -0700

    conditional rendering with early return

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -91,6 +91,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObject.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name}></img>
