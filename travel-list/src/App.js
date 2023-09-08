60136e9e7cf3847f88ad430c766955826e5c0d7e
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-09-08 14:52:00 -0700

    implement basic components and starter css

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      You have X items on your list, and you have already packed X (X)
+    </footer>
+  );
 }
