039002bc22aadde1917c97c437b23d5f1765143b
Cameron Anderson <cameron.anderson.j@gmail.com>
2023-09-10 18:16:58 -0700

    create stepmessage component

--- steps/src/App.js
+++ steps/src/App.js
@@ -31,9 +31,9 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+          </StepMessage>
 
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}><span>👈</span> Previous</Button>
@@ -50,4 +50,10 @@ function Button({ textColor, bgColor, onClick, children}) {
               style={{ backgroundColor: {bgColor}, color: {textColor} }}
               onClick={onClick}
             >{children}</button>
+}
+
+function StepMessage({step, children}) {
+  return <div className="message">
+    <h3>Step {step}</h3> {children}
+  </div>
 }
