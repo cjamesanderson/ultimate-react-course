b85cbd675c545a4cc8dc9599ad5c77de4f0da592
Cameron Anderson <cameron.anderson.j@gmail.com>
2024-03-22 17:47:30 -0700

    remove unused tempQuery variable

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -234,7 +234,6 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
-  const tempQuery = "Interstellar";
 
   useEffect(function () { 
     async function fetchMovies() {
