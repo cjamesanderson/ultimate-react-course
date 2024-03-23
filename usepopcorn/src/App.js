450e68123131a9e9658a5f961ef538494fbcdd9a
Cameron Anderson <cameron.anderson.j@gmail.com>
2024-03-22 18:29:21 -0700

    implement fetch and display of selected movie details

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,5 +1,6 @@
 import { is } from "@babel/types";
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -227,9 +228,60 @@ function ErrorMessage({message}) {
 }
 
 function MovieDetails({selectedId, onCloseMovie}) {
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(function() {
+    async function getMovieDetails() {
+      setIsLoading(true);
+      const res = await fetch(`https://www.omdbapi.com/?i=${selectedId}&apikey=${KEY}`);
+      const data = await res.json();
+      setMovie(data);
+      setIsLoading(false);
+    }
+    getMovieDetails();
+  }, [selectedId])
+
   return <div className="details">
+    {isLoading ? <Loader /> : 
+    <>
+    <header>
     <button className="btn-back" onClick={onCloseMovie}> &larr; </button>
-    {selectedId}</div>
+      <img src={poster} alt={`Poster of ${movie} movie`} />
+      <div className="details-overview">
+        <h2>{title}</h2>
+        <p>
+          {released} &bull; {runtime}
+        </p>
+        <p>{genre}</p>
+        <p><span>⭐</span>{imdbRating} IMDb rating</p>
+      </div>
+    </header>
+
+    <section>
+      <div className="rating">
+      <StarRating maxRating={10} size={24} />
+</div>
+      <p><em>{plot}</em></p>
+      <p>Starring {actors}</p>
+      <p>Directed by {director}</p>
+    </section>
+    </>
+}
+    </div>
 }
 
 const KEY = "7f8622c3";
