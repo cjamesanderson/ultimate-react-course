4d808470d0e176ba94d4c87fc7103cc586de70d7
Cameron Anderson <cameron.anderson.j@gmail.com>
2025-03-25 11:03:58 -0700

    add automatic focusing and resetting of source box

--- usepopcorn/src/Search.js
+++ usepopcorn/src/Search.js
@@ -1,10 +1,34 @@
+import { useEffect, useRef } from "react";
+
 export function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+    
+    function callback (e) {
+      if (document.activeElement === inputEl.current) return;
+
+      inputEl.current.focus()
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+  }, [setQuery]);
+  
+
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)} />
+      onChange={(e) => setQuery(e.target.value)} 
+      ref={inputEl}
+    />
   );
 }
